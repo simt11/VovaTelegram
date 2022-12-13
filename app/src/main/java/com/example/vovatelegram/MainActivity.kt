43a500ed26b1b69662b8c7841f17dbee11135ad5
simt11 <simt11@yandex.ru>
2022-12-13 17:42:43 +0300

    1) Включен viewBinding 2) Подключена библиотека mikepenz:materialdrawer 3) Созданы папки:  - models  - ui
    * Как заявляет автор курса это предпосылка к MVVP.
    4) Цвета стиля приведены к цветам телеграм. Сами цвета изменены в colors.xml
    5)

--- app/src/main/java/com/example/vovatelegram/MainActivity.kt
+++ app/src/main/java/com/example/vovatelegram/MainActivity.kt
@@ -3,11 +3,66 @@ package com.example.vovatelegram
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vovatelegram.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.Drawer
+import androidx.appcompat.widget.Toolbar
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawer: Drawer
+    private lateinit var header: AccountHeader
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        drawer = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(toolbar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(header)
+            .addDrawerItems(
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать группу")
+                    .withSelectable(false)
+            ).build()
+    }
+
+    private fun initFunc(){
+        setSupportActionBar(toolbar)
+        createHeader()
+
+    }
+
+    private fun createHeader(){
+        header = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem().withName("Vova B")
+                    .withEmail("+79225623475")
+            ).build()
+    }
+
+    private fun initFields(){
+        toolbar = binding.mainToolbar
+
     }
 }
